76ed47cfe0c2bd8daf023718af5ee3c0f0ce8e55
Alex <astro_studios@outlook.com>
2026-06-04 15:11:53 +0100

    fix: error checking and bu fix on cameraSystem

--- include/CameraSystem.h
+++ include/CameraSystem.h
@@ -3,11 +3,19 @@
 #include <vector>
 #include <string>
 
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+#include <chrono>
+
 #include "ps3eye.h"
 
 #define WIDTH 640
 #define HEIGHT 480
 #define FPS 60
+
+#define PERIOD (1.0 / FPS)
 
 class CameraSystem {
 public:
@@ -20,7 +28,9 @@ public:
 	cv::Mat* getCameraFrames();
 
 private:
-	std::vector<PS3EYECam::PS3EYERef>& devices;
+	std::vector<ps3eye::PS3EYECam::PS3EYERef> devices;
+
+	std::chrono::steady_clock::time_point lastFrameRead;
 
 	int numberOfCams = 0;
 
--- src/CameraSystem.cpp
+++ src/CameraSystem.cpp
@@ -1,32 +1,36 @@
 #include "CameraSystem.h"
 
+using namespace std::chrono;
+using namespace std;
+using namespace ps3eye;
+
 int CameraSystem::init() {
-	devices = ps3eye::PS3EYECam::getDevices(true);
-
-	cameraFrames = new cv::Mat[devices.size()]
-
+	devices = PS3EYECam::getDevices(true);
 	numberOfCams = devices.size();
+	cameraFrames = new cv::Mat[numberOfCams];
 
 	if (cameraFrames) {
 		for (int i = 0; i < numberOfCams; i++) {
-			cameraFrames[i] = cv::Mat::zeros(cv::Size(WIDTH, HEIGHT), CV_8UC3)
+			cameraFrames[i] = cv::Mat::zeros(cv::Size(WIDTH, HEIGHT), CV_8UC3);
 		}
 	}
 
-	for (int i = 0; i < devices.size(); i++) {
+	for (int i = 0; i < numberOfCams; i++) {
 
 		if (!devices[i]->init(WIDTH, HEIGHT, FPS)) {
-			std::cout << "could not init camera" << i << "\n";
+			cout << "could not init camera: " << i << "\n";
 		}
 		devices[i]->start();
 		devices[i]->setAutogain(true);
 		devices[i]->setAutoWhiteBalance(true);
 	}
+
+	lastFrameRead = high_resolution_clock::now();
 }
 
 CameraSystem::~CameraSystem() {
-	for (int i = 0; i < devices.size(); i++) {
-		if (devices[i].isInitialized()) {
+	for (int i = 0; i < numberOfCams; i++) {
+		if (devices[i]->isInitialized()) {
 			devices[i]->stop();
 		}
 	}
@@ -41,5 +45,25 @@ int CameraSystem::getNumberOfCameras() {
 }
 
 cv::Mat* CameraSystem::getCameraFrames() {
+	auto currentTime = high_resolution_clock::now();
+	auto diff = duration_cast<microseconds>(currentTime - lastFrameRead);
+
+	if (diff.count() < PERIOD) {
+		std::cout << "Reading Frames early\n";
+	}
+	else {
+		if (cameraFrames) {
+			for (int i = 0; i < devices.size(); i++) {
+				if (devices[i]->isInitialized()) {
+
+					devices[i]->getFrame(cameraFrames[i].data);
+				}
+			}
+		}
+		else {
+			std::cout << "Camera Frames not initialised\n";
+		}
+	}
+	
 	return cameraFrames;
 }
